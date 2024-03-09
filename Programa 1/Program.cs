7857390281adb9e564b54c1c0847aa0a09fd5c67
Cristian <cchamom2@miumg.edu.gt>
2024-03-09 12:33:55 -0600

    Subi el parcial a git hub

---
+++ Programa 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Bienvenido, por favor ingrea tu correo electronico de dominio gmail: ");
+
+        string correo = Console.ReadLine();
+
+          if (correo.EndsWith("@gmail.com"))
+        {
+            string nuevoC = correo.Replace("@gmail.com", "@miumg.edu.gt");
+            Console.WriteLine("El nuevo correo ekectronico generado es: " + nuevoC);
+        }
+          else
+        {
+            Console.WriteLine("El correo electronico no es valido por que no es de dominio gmail.");
+        }
+    }
+}
+
+
+
---
+++ Programa2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola bienvenido, ingresa el precio de 3 productos: ");
+
+        float[] precios = new float[3];
+
+     
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("Ingrese los precios de los productos" + (i + 1) + ": ");
+            precios[i] = float.Parse(Console.ReadLine());
+
+            if (precios[i] > 100.00)
+            {
+                float descuento = CalcularDescuento(precios[i]);
+                Console.WriteLine("El precio del producto " + (i + 1) + " sobrepasa el Q.100.00. de quetzales");
+                Console.WriteLine("Se hara un descuento de un 15%.");
+                Console.WriteLine("Precio con el descuento: Q" + (precios[i] - descuento));
+            }
+        }
+
+       
+        float totaldelacompra = 0;
+        float ahorroTotal = 0;
+        foreach (float precio in precios)
+        {
+            totaldelacompra += precio;
+            if (precio > 100.00)
+            {
+                ahorroTotal += CalcularDescuento(precio);
+            }
+        }
+
+        Console.WriteLine("Total de la compra, sin en el descuentos: Q" + totaldelacompra);
+        Console.WriteLine("Monto que el usuario que se ahorra por el descuentos: Q" + ahorroTotal);
+    }
+
+    static float CalcularDescuento(float precio)
+    {
+        return precio * 0.15f;
+    }
+}
+
